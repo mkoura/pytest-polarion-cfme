41b0fbb55f84288f5de3ad76f2c0ef3b0a3dbbb3
Martin Kourim <mkourim@redhat.com>
2017-05-16 11:56:27 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="pytest_polarion_cfme",
+    name='pytest_polarion_cfme',
     version='0.1.0',
-    url="NONE",
-    description="pytest plugin for collecting test cases and recording test results",
+    url='https://github.com/mkoura/pytest-polarion-cfme',
+    description='pytest plugin for collecting test cases and recording test results',
     long_description=open('README.rst').read().strip(),
     author='Martin Kourim',
     author_email='mkourim@redhat.com',
+    license='GPL',
     py_modules=['pytest_polarion_cfme'],
     install_requires=['pytest>=2.4.2'],
     entry_points={'pytest11': ['pytest_polarion_cfme = pytest_polarion_cfme']},
