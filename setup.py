184869de81ca0a8c80067a9b6c375a6558b40ee2
Martin Kourim <mkourim@redhat.com>
2017-06-02 16:39:49 +0200

    changes related to pypi upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytest_polarion_cfme',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/mkoura/pytest-polarion-cfme',
     description='pytest plugin for collecting test cases and recording test results',
     long_description=open('README.rst').read().strip(),
@@ -12,6 +12,13 @@ setup(
     py_modules=['pytest_polarion_cfme'],
     install_requires=['pytest>=2.4.2'],
     entry_points={'pytest11': ['pytest_polarion_cfme = pytest_polarion_cfme']},
-    keywords='py.test pytest',
-    classifiers=['Private :: Do Not Upload'],  # hack to avoid uploading to pypi
+    keywords=['polarion', 'py.test', 'pytest', 'testing'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Framework :: Pytest',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7']
 )
